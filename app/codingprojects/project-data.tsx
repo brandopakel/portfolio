3e23958e9a66a871b182eea834b8e08ff41c71d2
B P <bp@Bs-MacBook-Air.local>
2025-07-03 16:12:01 -0700

    Added crypto dash project to site project list.

--- app/codingprojects/project-data.tsx
+++ app/codingprojects/project-data.tsx
@@ -13,9 +13,9 @@ export const projects: Project[] = [
     url: "https://github.com/brandopakel/crypto-trading-engine",
   },
   {
-    title: "",
-    year: "",
-    description: "",
-    url: "",
+    title: "Crypto Dashboard",
+    year: "2025",
+    description: "A full user dashboard. You can access a full suite of coin data, as well as defi data and other user wallet data. You can manage your own coin holdings as well as interact with the trading view to incorporate technical strategies into your analysis. You can utilize the ML/AI platform built within to test user selected strategies, as well as let the dashboard trade for you.",
+    url: "https://github.com/brandopakel/crypto-dash",
   },
 ];
